0c0a308f4d395ea72aaf329af8946b1961194117
Christian Lins <christian@lins.me>
2013-08-01 15:14:03 +0200

    Remove obsolet (for now) JavaScript files

--- view_ids_reduce.js
+++
@@ -1,9 +0,0 @@
-function (key, values, rereduce) {
-    // Return the maximum numeric value.
-    var max = -Infinity
-    for (var i = 0; i < values.length; i++) {
-        if (values[i] > max || max == -Infinity)
-            max = values[i]
-    }
-    return max
-}
